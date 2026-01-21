e9c812426431f76ca17dca93477cc2ceb1ec8081
NasGRich <NasGRich@users.noreply.github.com>
2026-01-21 08:18:54 +0700

    feat: Add utility for generating contract PDFs from contract data, including HTML content and formatting.

--- src/utils/contract-pdf-generator.ts
+++ src/utils/contract-pdf-generator.ts
@@ -157,7 +157,7 @@ export function generateContractHTML(
             <th style="border: 1px solid #000; padding: 6px 4px; text-align: center; vertical-align: middle; line-height: 1.4;">Lớp học</th>
             <th style="border: 1px solid #000; padding: 6px 4px; text-align: center; vertical-align: middle; line-height: 1.4; width: 50px;">Số buổi</th>
             <th style="border: 1px solid #000; padding: 6px 4px; text-align: center; vertical-align: middle; line-height: 1.4;">Bắt đầu</th>
-            <th style="border: 1px solid #000; padding: 6px 4px; text-align: center; vertical-align: middle; line-height: 1.4;">Kết thúc</th>
+            <th style="border: 1px solid #000; padding: 6px 4px; text-align: center; vertical-align: middle; line-height: 1.4;">Ngày KT dự kiến</th>
             <th style="border: 1px solid #000; padding: 6px 4px; text-align: center; vertical-align: middle; line-height: 1.4;">Giá trị</th>
             <th style="border: 1px solid #000; padding: 6px 4px; text-align: center; vertical-align: middle; line-height: 1.4; width: 50px;">ƯĐHĐ</th>
             <th style="border: 1px solid #000; padding: 6px 4px; text-align: center; vertical-align: middle; line-height: 1.4;">Thành tiền</th>
